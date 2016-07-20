3ddb13d987c1f2f17d839df7eed98a761885dba0
shinichi-sakai <shinichi.sakai@weblio.co.jp>
2016-07-20 17:16:19 +0900

    .

--- marker_experiment/js/script.js
+++ marker_experiment/js/script.js
@@ -3,45 +3,42 @@ var highlightTxt = '';
 $(document).ready(function() {
 
   // mouseup時のアクション
-  $('body').mouseup(function() {
-
-    // カラーピッカーを表示/非表示させる。
-    toggleColorPicker();
+  $('body').mouseup(function(e) {
 
     // ハイライトするテキストをセット
-    highlightTxt = getSelectedText();
+    highlightTxt = getSelectedTextAndToggleColorPicker(e);
 
-　　　
   });
 
+  // colorpickerクリック時のアクション
   $('.pkiepick').click(function() {
 
     // 付与するクラス名を取得する
     var colorClassName = geColorClassName(this);
-    alert(highlightTxt);
 
-    // if (highlightText.length > 0)) {
-    //   // ターゲット文字列をハイライトする
-    //   highlightTargetedText(highlightText, colorClassName);
-    // }
+    if (colorClassName.length > 0 && highlightTxt.length > 0) {
+      // ターゲット文字列を指定した色でハイライトする
+      highlightTargetedText(colorClassName);
+    }
+  });
+
+  //　ハイライト箇所にマウスオーバーした場合のアクション
+  $(document)
+  .on('mouseenter', '.marker', function(e) {
+      toggleColorPicker(e);
+      $('#pick-del').show();
+      // $(this).contents().unwrap();
+
+  })
+  .on('mouseleave', '.marker', function(e) {
 
   });
 });
 
-function getSelectedText() {
-  var text = "";
-  if (window.getSelection) {
-    text = window.getSelection().toString();
-  } else if (document.selection && document.selection.type != "Control") {
-    text = document.selection.createRange().text;
-  }
-  return text;
-}
-
-function highlightTargetedText(targetText, colorClassName) {
-  $('p:contains(' + targetText + ')').each(function(){
+function highlightTargetedText(colorClassName) {
+  $('p:contains(' + highlightTxt + ')').each(function(){
     var scope = $(this).html();
-    $(this).html(scope.replace(targetText,'<span class="' + colorClassName + '">' + targetText + '</span>'));
+    $(this).html(scope.replace(highlightTxt,'<span class="marker ' + colorClassName + '">' + highlightTxt + '</span>'));
   });
 }
 
@@ -59,6 +56,31 @@ function geColorClassName(obj) {
   else return '';
 }
 
-function toggleColorPicker() {
-  $('.color-picker-wrp').toggle();
+function getSelectedTextAndToggleColorPicker(e) {
+  var text = '';
+  if (window.getSelection) {
+    text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+    text = document.selection.createRange().text;
+  }
+
+  if (text.length > 0) {
+    $('.color-picker-wrp').show().css('top', e.clientY).css('left', e.clientX);
+    return text;
+  }
+
+  $('.color-picker-wrp').hide();
+  return highlightTxt;
+
+}
+
+/**
+ * カラーピッカーを表示/非表示させる
+ */
+function toggleColorPicker(e) {
+  if (!$('.color-picker-wrp').is(':visible')) {
+    $('.color-picker-wrp').show().css('top', e.clientY).css('left', e.clientX);
+  } else {
+    $('.color-picker-wrp').hide();
+  }
 }
