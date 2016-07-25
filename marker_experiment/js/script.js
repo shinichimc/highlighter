923eabab63268da8408225a5b6b6ef05aa90a369
shinichi-sakai <shinichi.sakai@weblio.co.jp>
2016-07-25 18:34:28 +0900

    fire

--- marker_experiment/js/script.js
+++ marker_experiment/js/script.js
@@ -1,110 +1,149 @@
 (function() {
-    var highlightTxt = '';
+    var activeHighlightTxt = '';
+    var activeColor = '';
+    var activeHighLightId = '';
+
     var ID_PFIX = 'highLight_id_';
-
-    function pH () {
-        $('#constantTargetText').text(highlightTxt);
-    }
-
-    function pC(pureContent) {
-        $('#constantPureContent').text(pureContent);
-    }
 
     $(document).ready(function() {
 
+        // 保存しているハイライトを出現させる
         highlightOnPageLoad();
-        //highlightClear();
 
-        /*%%%*/pH();
-        // mouseup時のアクション
+        //////////////////////////
+        // MOUSEUP 時のアクション
+        //////////////////////////
         $('body').mouseup(function(e) {
             // ハイライトするテキストをセット
-            highlightTxt = getSelectedTextAndToggleColorPicker(e);
-            /*%%%*/pH();
+            activeHighlightTxt = getSelectedTextAndToggleColorPicker(e);
+
         });
 
-        // colorpickerクリック時のアクション
-        $(document).on('click', '.pkiepick', function() {
-
-            // 付与するクラス名を取得する
-            var colorClassName = geColorClassName(this);
-
-            if (colorClassName.length > 0 && highlightTxt.length > 0) {
-                // ターゲット文字列を指定した色でハイライトする
-                highlightTargetedText(colorClassName, true);
-            }
-        });
-
-        //　ハイライト箇所にマウスオーバーした場合のアクション
+        //////////////////////////
+        // ハイライトに MOUSEOVER た場合のアクション
+        // 1) 色変更時
+        // 2) 削除時
+        //////////////////////////
         $(document).on('mouseenter', '.marker', function(e) {
             toggleColorPicker(e);
             $('#pick-del').show();
 
-            highlightTxt = $(this).prop('outerHTML');
-            /*%%%*/pH();
+            activeHighlightTxt = $(this).prop('outerHTML');
         });
 
-        // delボタンをクリックした時のアクション
+        //////////////////////////
+        // COLORPICKERクリック時のアクション
+        //　1) 新規登録時
+        // 2) 色変更時
+        //////////////////////////
+        $(document).on('click', '.pkiepick', function() {
+
+            // 付与するクラス名を取得する (red, green, yellow, blue)
+            activeColor = geColorClassName(this);
+
+            if (activeColor.length > 0 && activeHighlightTxt.length > 0) {
+                // ターゲット文字列を指定した色でハイライトする
+                highlightTargetedText(false);
+            }
+        });
+
+        //////////////////////////
+        // DELETEボタンをクリックした場合のアクション
+        //////////////////////////
         $(document).on('click', '#pick-del', function() {
             $('body').each(function()　{
 
                 var scope = $(this).html();
 
-                var idAndPureContents = fetchIdAndPureContents();
+                var idAndPureContents = fetchIdAndPureContents(activeHighlightTxt);
 
                 if (idAndPureContents.length > 2) {
-                    var highlightId = idAndPureContents[1];
+                    activeHighLightId = idAndPureContents[1];
                     var pureContent = idAndPureContents[2];
                     // 対象のハイライトを削除する
-                    $(this).html(scope.replace(highlightTxt, pureContent));
-                    console.log(highlightId);
-                    console.log(pureContent);
-                    removeItem(highlightId);
+                    $(this).html(scope.replace(activeHighlightTxt, pureContent));
+
+                    removeItem(activeHighLightId);
                 }
             });
         });
     });
 
-    /**
-    * ハイライトする
-    */
-    function highlightTargetedText(colorClassName, storeData) {
+    // ページロード時に保存されているハイライトを出現させる
+    function highlightOnPageLoad() {
+        if (localStorage) {
+            var storage = localStorage;
+            // ストレージの内容を順番に取り出し、リストに整形（1）
+            for (var i = 0; i < storage.length; ++i) {
+                // i番目のキーを取得（2）
+                var k = storage.key(i);
+                // 指定されたキーの値を取得（3）
 
-        $('body').each(function(){
-            var scope = $(this).html();
+                var data = JSON.parse(storage.getItem(k));
+                activeHighlightTxt = data['targetTxt'];
+                activeColor = data['color'];
+                activeHighLightId = k;
 
-            var pureContent = fetchIdAndPureContents();
-
-            // リプレースするテキスト
-            var textToInsert = '';
-
-            if (pureContent != null && pureContent.length > 1) {
-                // すでにハイライトがなされている場合に色を変更する
-                textToInsert = pureContent[1];
-            }　else {
-                // 未だハイライトが付いていない時にハイライトを施す
-                textToInsert = highlightTxt;
+                highlightTargetedText(true);
             }
-            var highlightId = ID_PFIX + getRandomString();
-            // 置換処理をおこなう( = ハイライトする)
-            $(this).html(scope.replace(highlightTxt,'<span class="marker ' + highlightId + ' ' + colorClassName + '">' + textToInsert + '</span>'));
-            if (storeData) {
-                store(highlightId , highlightTxt, colorClassName);
-            }
-
-        });
+        }
     }
 
     /**
-    * <span>で囲まれたコンテンツを取得
+    * ハイライトする
+    *
+    * 1) 新規でハイライトする場合
+    * 2) ページロード時
+    * 3) 色を変更する場合
     */
-    function fetchIdAndPureContents() {
-        var regexp = new RegExp('^<span.+?(' + ID_PFIX + '[^\\s]+?)\\s.+?>([\\s\\S]+)<\/span>$'); //[\s\S] = 改行を含む任意の一文字
-        var pureContent = highlightTxt.match(regexp)
-        if (pureContent) {
-            return pureContent;
-        }
-        return '';
+    function highlightTargetedText(onPageLoad) {
+
+        $('body').each(function() {
+
+            var scope = $(this).html();
+
+            // 色変更時のみ取得可能
+            var idAndPureContents = fetchIdAndPureContents(activeHighlightTxt);
+
+            // 挿入するテキスト
+            var textToInsert = '';
+
+
+            ////////////////////////////
+            // 色を変更する場合
+            ////////////////////////////
+            if (idAndPureContents.length > 2) {
+                alert('色変更');
+
+                activeHighLightId = idAndPureContents[1];
+                textToInsert = idAndPureContents[2];
+
+            } else if (onPageLoad) {
+            ////////////////////////////
+            // ページロード時
+            ///////////////////////////
+                alert('ページロード');
+                activeHighLightId = activeHighLightId;
+                textToInsert = activeHighlightTxt;
+            } else {
+            ////////////////////////////
+            // 新規にハイライトを施す場合
+            ////////////////////////////
+                alert('新規');
+                activeHighLightId = ID_PFIX + getRandomString();
+                textToInsert = activeHighlightTxt;
+            }
+
+
+            // 置換処理をおこなう( = ハイライトする)
+            $(this).html(scope.replace(activeHighlightTxt,'<span class="marker ' + activeHighLightId + ' ' + activeColor + '">' + textToInsert + '</span>'));
+
+            // ストレージに保存
+            if (!onPageLoad) {
+                store(activeHighLightId , textToInsert, activeColor);
+            }
+
+        });
     }
 
     function geColorClassName(obj) {
@@ -150,7 +189,7 @@
         // 文字を取得できな買った場合
         $('#pick-del').hide();
         $('.color-picker-wrp').hide();
-        return highlightTxt;
+        return activeHighlightTxt;
     }
 
     /**
@@ -164,46 +203,6 @@
         }
     }
 
-
-// SET to local storage
-function store(key, targetTxt, color) {
-    // ストレージが利用できるかをチェック
-    if (localStorage) {
-      // ローカルストレージを呼び出し
-        var storage = localStorage;
-
-        var datalist = {
-            targetTxt: targetTxt,
-            color: color
-        }
-
-      // ストレージにキーと値をセット
-      storage.setItem(key, JSON.stringify(datalist));
-    }
-}
-
-
-// GET from local storage
-function getItem(key) {
-    // ストレージが利用できるかをチェック
-    if (localStorage) {
-      // ローカルストレージをセット
-      var storage = localStorage;
-      // ストレージからキーlastAccessedを取得
-      return storage.getItem(key);
-    }
-}
-
-// REMOVE from local storage
-function removeItem(key) {
-    // ストレージが利用できるかをチェック
-    if (localStorage) {
-      // ローカルストレージをセット
-      var storage = localStorage;
-      // ストレージからキーlastAccessedを取得
-      storage.removeItem(key);
-    }
-}
 
 // ID作成用のランダム文字列を生成する
 function getRandomString() {
@@ -222,23 +221,6 @@ function getRandomString() {
     return r;
 }
 
-// ページロード時に保存されているハイライトを出現させる
-function highlightOnPageLoad() {
-    if (localStorage) {
-        var storage = localStorage;
-        // ストレージの内容を順番に取り出し、リストに整形（1）
-        for (var i = 0; i < storage.length; ++i) {
-            // i番目のキーを取得（2）
-            var k = storage.key(i);
-            // 指定されたキーの値を取得（3）
-
-            var data = JSON.parse(storage.getItem(k));
-            highlightTxt = data['targetTxt'];
-
-            highlightTargetedText(data['color'], false);
-        }
-    }
-}
 
 // localstorageを全て削除(デバッグ用)
 function highlightClear() {
@@ -248,4 +230,56 @@ function highlightClear() {
     }
 }
 
+/**
+* ある文字列がspanで囲まれている場合持っているハイライトIDとタグ内の文字列を返す。
+*/
+function fetchIdAndPureContents(txt) {
+    var regexp = new RegExp('^<span.+?(' + ID_PFIX + '[^\\s]+?)\\s.+?>([\\s\\S]+)<\/span>$'); //[\s\S] = 改行を含む任意の一文字
+    var result = txt.match(regexp)
+    if (result) {
+        return result;
+    }
+    return '';
+}
+
 })();
+
+// ローカルストレージに保存する
+function store(key, targetTxt, color) {
+    // ストレージが利用できるかをチェック
+    if (localStorage) {
+      // ローカルストレージを呼び出し
+        var storage = localStorage;
+
+        var datalist = {
+            targetTxt: targetTxt,
+            color: color
+        }
+
+      // ストレージにキーと値をセット
+      storage.setItem(key, JSON.stringify(datalist));
+    }
+}
+
+
+// ローカルストレージからキーに対する値を取得する
+function getItem(key) {
+    // ストレージが利用できるかをチェック
+    if (localStorage) {
+      // ローカルストレージをセット
+      var storage = localStorage;
+      // ストレージからキーlastAccessedを取得
+      return storage.getItem(key);
+    }
+}
+
+// ローカルストレージから指定してデータを削除する
+function removeItem(key) {
+    // ストレージが利用できるかをチェック
+    if (localStorage) {
+      // ローカルストレージをセット
+      var storage = localStorage;
+      // ストレージからキーlastAccessedを取得
+      storage.removeItem(key);
+    }
+}
